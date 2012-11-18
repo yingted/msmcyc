40441344e3b281e553fe84474a7f2c5a10691f8f
Ted Ying <yingted@gmail.com>
2012-11-18 09:22:15 -0500

    updates templatetag

--- site/msmcyc/settings.py
+++ site/msmcyc/settings.py
@@ -118,7 +118,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ajax_forms'
+    'ajax_forms',
+    'msmcyc',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- site/msmcyc/views.py
+++ site/msmcyc/views.py
@@ -8,15 +8,11 @@ from django.template import TemplateDoesNotExist
 from django.shortcuts import redirect
 import re
 useful=re.compile(r'^/(?!base_)([^\.]*?)(?:\.html)?$')
-def updates():
-	return Update.all().order("-added").run(limit=3)
 def index(request):
 	match=useful.match(request.path)
 	if match:
 		try:
-			return render(request,(match.group(1)or"index")+".html",{
-				"updates":updates(),
-			})
+			return direct_to_template(request,(match.group(1)or"index")+".html")
 		except TemplateDoesNotExist as e:
 			logging.error(e)
 	return redirect("/static"+request.path)
@@ -56,23 +52,19 @@ def contact(request):
 			if form.cleaned_data["cc_sender"]:
 				message.cc="%s <%s>"%tuple(map(form.cleaned_data.get,("name","sender")))
 			message.send()
-			return render(request,"contact_success.html",{
-				"updates":updates(),
-			})
+			return direct_to_template(request,"contact_success.html")
 	else:
 		form=ContactForm()
 	return render(request,"contact.html",{
-		"updates":updates(),
 		"form":form,
 	})
 
 from datetime import datetime
 def events(request):
 	return render(request,"events.html",{
-		"updates":updates(),
 		"events":Event.all()
 			.filter("when >",datetime.now())
-			.order("when")
+			.order("when"),
 	})
 
 from google.appengine.ext.db.djangoforms import ModelForm
