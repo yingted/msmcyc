6f3d7e8c24cc6b4ef2a7c8622a1887dd61e8c19c
Ted Ying <yingted@gmail.com>
2012-11-23 05:32:39 -0500

    wai-aria button role

--- src/add.sh
+++ src/add.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 while [ "$#" -gt 0 ]
 do
-	cp "$1" ./
-	echo "$1" >> copy.sh
+	cp -nv"$1" ./ && echo "	grep -v '[^[:alnum:]]*vim:' ${1##*/} > $1" >> Makefile
 	shift
 done
--- src/copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-tail -n+7 "$0" | while read x
-do
-	cp -uv "${x%*/}" "$x"
-done
-exit 0
-../site/static/css/main.css
-../site/msmcyc/templates/base_home.html
