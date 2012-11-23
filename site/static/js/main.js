33e5374566554da5d792ccfb66fd1faa88fcd135
Ted Ying <yingted@gmail.com>
2012-11-23 05:35:46 -0500

    remove duplicate code

--- site/static/js/main.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$("a:not(.click)").filter(function(i,e){
-		return/^https?:$/.test(e.protocol)&&e.host!=location.host;
-	}).addClass("external");
-});
-/* vim: set ts=4 sw=4: */
--- src/add.sh
+++ src/add.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while [ "$#" -gt 0 ]
 do
-	cp -nv"$1" ./ && echo "	grep -v '[^[:alnum:]]*vim:' ${1##*/} > $1" >> Makefile
+	cp -nv"$1" ./ && echo "	grep -v '[^[:alnum:]]*vim:' ${1##*/} > $1" >> Makefile && git rm --cached "$1"
 	shift
 done
