0408a25eb8fb9e41095933b7e62e78c97fb81a34
Ted Ying <yingted@gmail.com>
2012-11-26 16:29:24 -0500

    rm script

--- src/rm.sh
+++ src/rm.sh
@@ -2,6 +2,6 @@
 while [ "$#" -gt 0 ]
 do
 	path="$(lua - <<< 'for line in io.lines("../.gitignore")do if arg[1]==line:sub(-#arg[1])then print(line)end end' "$1")"
-	git add "../$path" && sed -i "$(grep -nFx "$path" ../.gitignore | cut -d: -f1)d" ../.gitignore && rm "$1"
+	sed -i "$(grep -nFx "$path" ../.gitignore | cut -d: -f1)d" ../.gitignore && sed -i "/ $1 > /d" Makefile && git rm "../$path" && rm "$1"
 	shift
 done
