0cf58f541f7e6a0ecbc6b57c5a3d72d37115e908
Ted Ying <yingted@gmail.com>
2012-12-07 20:41:37 -0500

    reorder email

--- src/views.py
+++ src/views.py
@@ -31,11 +31,11 @@ class ContactForm(forms.Form):
 	name=forms.CharField(max_length=1000)
 	sender=MyEmailField(label="Your email")
 	about=forms.ChoiceField(choices=(
+		("Sponsors","Becoming a sponsor"),
+		("Volleyball","MS Volleyball Tournament"),
 		("AboutUs","General information"),
 		("HelpMe","I need help with..."),
 		("Website","Website issues"),
-		("Sponsors","Becoming a sponsor"),
-		("Volleyball","MS Volleyball Tournament"),
 		("Other","Other"),
 	))
 	message=forms.CharField(widget=forms.widgets.Textarea)
