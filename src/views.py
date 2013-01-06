d230bbda7ad07d96b2fd44b980fbc62365338f4c
Ted Ying <yingted@gmail.com>
2013-01-06 12:17:18 -0500

    stupid bug

--- src/views.py
+++ src/views.py
@@ -67,7 +67,6 @@ from datetime import datetime
 from django.template import Template,RequestContext
 def events(request):
 	events=Event.all()\
-		#.filter("when >",datetime.now())\
 		.order("when")\
 		.fetch(None)
 	for event in events:
